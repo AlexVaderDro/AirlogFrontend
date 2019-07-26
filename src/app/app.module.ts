fac8812412dbbaa93649b4423c41704119af2589
AlexVaderDro <51241875+AlexVaderDro@users.noreply.github.com>
2019-07-26 16:10:04 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './table-view/table-view.component';
+import { TextViewComponent } from './text-view/text-view.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TableViewComponent, TextViewComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
