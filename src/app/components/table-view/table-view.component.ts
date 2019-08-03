c002fea2f89e4eac99338e314a8c5eb0886d4583
AlexVaderDro <51241875+AlexVaderDro@users.noreply.github.com>
2019-08-03 08:32:14 +0400

    added routing and also unfinished get request

--- src/app/components/table-view/table-view.component.ts
+++ src/app/components/table-view/table-view.component.ts
@@ -1,94 +1,4 @@
-/*
-import {HttpClient} from '@angular/common/http';
-import {Component, ViewChild, AfterViewInit} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {merge, Observable, of as observableOf} from 'rxjs';
-import {catchError, map, startWith, switchMap} from 'rxjs/operators';
-
-/!**
- * @title Table retrieving data through HTTP
- *!/
-@Component({
-  selector: 'app-table-view',
-  styleUrls: ['./table-view.component.css'],
-  templateUrl: './table-view.component.html',
-})
-export class TableViewComponent implements AfterViewInit {
-  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
-  exampleDatabase: ExampleHttpDatabase | null;
-  data: GithubIssue[] = [];
-
-  resultsLength = 0;
-  isLoadingResults = true;
-  isRateLimitReached = false;
-
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-
-  constructor(private _httpClient: HttpClient) {}
-
-  ngAfterViewInit() {
-    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
-
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.exampleDatabase!.getRepoIssues(
-            this.sort.active, this.sort.direction, this.paginator.pageIndex);
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = false;
-          this.resultsLength = data.total_count;
-
-          return data.items;
-        }),
-        catchError(() => {
-          this.isLoadingResults = false;
-          // Catch if the GitHub API has reached its rate limit. Return empty data.
-          this.isRateLimitReached = true;
-          return observableOf([]);
-        })
-      ).subscribe(data => this.data = data);
-  }
-}
-
-export interface GithubApi {
-  items: GithubIssue[];
-  total_count: number;
-}
-
-export interface GithubIssue {
-  created_at: string;
-  number: string;
-  state: string;
-  title: string;
-}
-
-/!** An example database that the data source uses to retrieve data for the table. *!/
-export class ExampleHttpDatabase {
-  constructor(private _httpClient: HttpClient) {}
-
-  getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
-    const href = 'https://api.github.com/search/issues';
-    const requestUrl =
-      `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page + 1}`;
-
-    return this._httpClient.get<GithubApi>(requestUrl);
-  }
-}
-*/
-import {HttpClient} from '@angular/common/http';
-import {merge, Observable, of as observableOf} from 'rxjs';
-import {catchError, map, startWith, switchMap} from 'rxjs/operators';
-import {AfterViewInit, Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Log} from '../../models/log';
 import {LogService} from '../../services/log.service';
 
@@ -97,28 +7,18 @@ import {LogService} from '../../services/log.service';
   styleUrls: ['./table-view.component.css'],
   templateUrl: './table-view.component.html',
 })
-export class TableViewComponent implements AfterViewInit {
+export class TableViewComponent implements OnInit {
   displayedColumns = ['date', 'message'];
-  exampleDatabase: LogService | null;
-  logs: Log[] = [];
+  logs: Log[];
 
-  constructor(private logService: LogService, private httpClient: HttpClient) {}
+  constructor(private logService: LogService) {}
 
-  /*getLogs(): void {
+  ngOnInit() {
+    this.getLogs();
+  }
+
+  getLogs(): void {
     this.logService.getLogs().subscribe(logs => this.logs = logs);
-  }*/
-
-  ngAfterViewInit() {
-    this.exampleDatabase = new LogService(this.httpClient);
-
-
-    merge()
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          return this.exampleDatabase.getLogs();
-        })
-      ).subscribe(logs => this.logs = logs);
   }
 }
 
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TableViewComponent } from '../components/table-view/table-view.component';
 import { TextViewComponent } from '../components/text-view/text-view.component';
 import { RouterModule, Routes } from '@angular/router';
