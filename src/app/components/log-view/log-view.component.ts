32bc6c2b80a48e0b13ee5f8fa9de3bb745dc563e
AlexVaderDro <51241875+AlexVaderDro@users.noreply.github.com>
2019-08-08 18:59:20 +0400

    bug fix

--- src/app/components/log-view/log-view.component.ts
+++ src/app/components/log-view/log-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ChangeDetectorRef} from '@angular/core';
 import {Log} from '../../models/log';
 import {LogService} from '../../services/log.service';
 import {SourceService} from '../../services/source.service';
@@ -12,25 +12,31 @@ export class LogViewComponent implements OnInit {
 
   protected logs: Log[] = [];
   protected sources: string[];
-  protected currentSource: string;
+  @Input() protected currentSource: string;
 
   constructor(protected logService: LogService, protected sourceService: SourceService) {}
 
-  protected getLogs(): void {
+  protected getLogs() {
     this.logService.getLogs().subscribe(logs => this.logs = logs);
+  }
+
+  protected getLogsBySource(source: string): void {
+    this.logService.getLogsBySource(source).subscribe(logs => this.logs = logs);
+    console.log(this.logs);
   }
 
   protected getSources(): void {
     this.sourceService.getSources().subscribe(sources => this.sources = sources);
   }
 
-  protected setCurrentSource(selectedSource: string) {
+  protected setCurrentSourceAndUpdateLogs(selectedSource: string) {
     this.currentSource = selectedSource;
+    console.log(this.currentSource);
+    this.getLogsBySource(this.currentSource);
   }
 
   ngOnInit() {
     this.getSources();
     this.getLogs();
   }
-
 }
--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,20 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent {
 
   @Input() data: string[];
   @Output() selectedData = new EventEmitter<string>();
 
+  constructor() {}
+
   selectData(event): void {
     this.selectedData.emit(event.target.value);
   }
-
-  constructor() {}
-
-  ngOnInit() {}
 }
