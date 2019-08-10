964464d74078de62f71558525fad1ab3e6645203
Vladimir <v.bisenov@mail.ru>
2019-08-10 14:40:09 +0400

    log saving from table

--- src/app/components/table-view/table-view.component.ts
+++ src/app/components/table-view/table-view.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Log} from '../../models/log';
-import {MatPaginator, MatTableDataSource} from "@angular/material";
+import {saveAs} from 'file-saver';
+import {HttpService} from "../../services/http-service/http.service";
 
 @Component({
   selector: 'app-table-view',
@@ -9,12 +10,30 @@ import {MatPaginator, MatTableDataSource} from "@angular/material";
 })
 export class TableViewComponent implements OnInit {
   @Input() logs: Log[];
+  logsToFile: Log[] = [];
+  file: File;
+  strLogs: string;
 
   displayedColumns = ['source', 'dateTime', 'message'];
 
-  constructor() {
+  constructor(protected httpService: HttpService) {
   }
 
   ngOnInit(): void {
+  }
+
+  getLogsByDateAndSource(date: string, source: string): Log[] {
+    this.httpService.getLogsByDateAndSource(date, source).subscribe(logs => this.logsToFile = logs);
+    console.log(this.logsToFile);
+    return this.logsToFile;
+  }
+
+  save(date: string, source: string): void {
+    console.log(this.getLogsByDateAndSource(date, source));
+    for (let log of this.logsToFile){
+      this.strLogs += log.dateTime +" "+log.source+" "+log.message+"\n"
+    }
+    this.file = new File([this.strLogs], "logs.txt", {type: "text/plain;charset=utf-8"});
+    saveAs(this.file);
   }
 }
