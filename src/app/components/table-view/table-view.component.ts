56603e47ee6cffbfee67258b53b2bc31ae57fbb7
Vladimir <v.bisenov@mail.ru>
2019-08-22 10:11:14 +0400

    added "save logs" button

--- src/app/components/table-view/table-view.component.ts
+++ src/app/components/table-view/table-view.component.ts
@@ -26,33 +26,4 @@ export class TableViewComponent implements OnInit {
       this.httpService.currentPage,
       this.httpService.pageSize);
   }
-
-  // save(dateStart: string, dateEnd?: string): void { // dateEnd - optional parameter
-  //   if (isUndefined(dateEnd)) {
-  //     dateEnd = dateStart + 300000;
-  //   }
-  //   let logsToFile: Log[];
-  //   let strLogs: '';
-  //
-  //   dateStart = this.minusMillisecond(dateStart);
-  //
-  //   this.httpService.getLogsByDate(dateStart, dateEnd, this.httpService.currentSource, 0, 100).subscribe(logs => {
-  //     logsToFile = logs;
-  //     for (let log of logsToFile) {
-  //       let dateInLong = new Date(log.dateTime);
-  //       let dateInString = dateInLong.toDateString() + ' ' + dateInLong.toTimeString();
-  //       strLogs += dateInString + ' ' + log.source + ' ' + log.message + '\n';
-  //     }
-  //     let dateInLong = new Date(dateStart);
-  //     let dateInString = dateInLong.toDateString() + ' ' + dateInLong.toTimeString();
-  //     let file = new File([strLogs], 'logs_after_' + dateInString + '.txt', {type: 'text/plain;charset=utf-8'});
-  //     saveAs(file);
-  //   });
-  // }
-
-  private minusMillisecond(date: string): string {
-    let d = new Date(date);
-    d.setMilliseconds(d.getMilliseconds() - 1);
-    return d.getTime().toString();
-  }
 }
--- src/app/services/http-service/log.service.ts
+++ src/app/services/http-service/log.service.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs';
 import {Log} from '../../models/log';
 import {environment} from '../../../environments/environment';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {saveAs} from 'file-saver';
 import {isUndefined} from 'util';
 import {removeSummaryDuplicates} from "@angular/compiler";
 
@@ -10,11 +11,14 @@ import {removeSummaryDuplicates} from "@angular/compiler";
   providedIn: 'root'
 })
 export class LogService {
+  
+  private _logs: Log[];
+
   private _currentSource: string;
   private _currentPage: number = 1;
-  private _totalItems: number;
   private _pageSize: number = 20;
-  private _logs: Log[];
+  private _totalItems: number;
+
   private _dateStart: string = (Date.now() - 86400000).toString(); //minus day
   private _dateEnd: string = (Date.now() + 86400000).toString();   //plus day
 
@@ -116,6 +120,31 @@ export class LogService {
       }
       console.log(url + " " + this.totalItems);
       this.httpClient.get<Log[]>(url, options).subscribe(logs => this.logs = logs);
+    });
+  }
+
+  private getLogsToSave(start: string, end: string, source: string, pageNum: number, quantity: number): Observable<Log[]>{
+    let url = "";
+    if (source == undefined || source == 'not specified') {
+      url = `${environment.url}/logs?start=${start}&end=${end}&pageNum=${pageNum}&pageSize=${quantity}`;
+    } else {
+      url = `${environment.url}/logs?start=${start}&end=${end}&source=${source}&pageNum=${pageNum}&pageSize=${quantity}`;
+    }
+    console.log(url + " " + this.totalItems);
+
+    return this.httpClient.get<Log[]>(url, options);
+  }
+
+  save(): void {
+    let strLogs = "";
+    this.getLogsToSave(this.dateStart, this.dateEnd, this.currentSource, 1, this.totalItems).subscribe(logs => {
+      for (let log of logs) {
+        let dateInLong = new Date(log.dateTime);
+        let dateInString = dateInLong.toDateString() + ' ' + dateInLong.toTimeString();
+        strLogs += dateInString + ' ' + log.source + ' ' + log.message + '\n';
+      }
+      let file = new File([strLogs], 'logs.txt', {type: 'text/plain;charset=utf-8'});
+      saveAs(file);
     });
   }
 }
