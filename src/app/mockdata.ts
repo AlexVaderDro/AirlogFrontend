5305dcdf777a59e7744bbe0439584b0089f3f276
AlexVaderDro <51241875+AlexVaderDro@users.noreply.github.com>
2019-08-24 12:10:21 +0400

    links coping and routing, refactoring

--- src/app/mockdata.ts
+++ src/app/mockdata.ts
@@ -1,16 +1,16 @@
 import {Log} from './models/log';
 
-export const MOCK_DATA: Log[] = [
-  {source: 'microservice1', id: 134, dateTime: '12', message: 'dhjsdiggheruhie'},
-  {source: 'microservice1', id: 164, dateTime: '2', message: 'qhrthmbfhfd'},
-  {source: 'microservice1', id: 131, dateTime: '22', message: 'jgkajgjkfnvjkn'},
-  {source: 'microservice1', id: 834, dateTime: '55', message: 'jhjregeioqphwojbfj'},
-  {source: 'microservice1', id: 174, dateTime: '55', message: 'jgiovjeigwoejgui'},
-  {source: 'microservice1', id: 344, dateTime: '77', message: 'werweoifhiunvjaidohg'}
-];
-
 export const MOCK_SOURCE_LIST: string[] = [
   'Data Receiver 3.5.1',
   'Payment Supporter 1.0.2',
   'Access Defender 4.0'
+];
+
+export const MOCK_DATA: Log[] = [
+  {source: MOCK_SOURCE_LIST[0], id: 1, dateTime: '1500000000001', message: 'it\'s OK'},
+  {source: MOCK_SOURCE_LIST[1], id: 2, dateTime: '1500000000002', message: 'it isn\'t OK'},
+  {source: MOCK_SOURCE_LIST[2], id: 3, dateTime: '1500000000003', message: 'it\'s OK'},
+  {source: MOCK_SOURCE_LIST[0], id: 4, dateTime: '1500000000004', message: 'it isn\'t OK'},
+  {source: MOCK_SOURCE_LIST[1], id: 5, dateTime: '1500000000005', message: 'it\'s OK'},
+  {source: MOCK_SOURCE_LIST[2], id: 6, dateTime: '1500000000006', message: 'it isn\'t OK'}
 ];
